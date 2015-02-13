7af98c76b4583ce9c35d62eaa6d022116aaabc31
Jacob Stanley <jacob@stanley.io>
2015-02-13 22:20:57 +1100

    Can write simple class with empty method

---
+++ examples/Main.java
@@ -0,0 +1,31 @@
+class Main { //implements Comparable {
+    //static int foo = 1;
+    //static float bar = 2.5f;
+
+    //too hard basket :)
+    //long baz = 3L;
+    //double quxx = 4.8;
+
+    public static int main(String[] args) {
+        //int z = add(foo, sub(3, 2));
+        //System.out.println("Hello World!");
+        //return z;
+        return 42;
+    }
+
+    //public static int add(int x, int y) {
+    //    return x + y;
+    //}
+
+    //public static int sub(int x, int y) {
+    //    return x - y;
+    //}
+
+    //public static int mul(int x, int y) {
+    //    return x * y;
+    //}
+
+    //public int compareTo(Object o) {
+    //    return -1;
+    //}
+}
