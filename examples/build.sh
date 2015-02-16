4db4906dd25ca035c6c7e44b05a71335112eb2c6
Jacob Stanley <jacob@stanley.io>
2015-02-16 23:25:19 +1100

    Generated a bunch of byte code examples

---
+++ examples/build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+pretty() {
+    echo "javap -l -p -c -s ${1}.class > ${1}.bytecode"
+    javap -l -p -c -s ${1}.class > ${1}.bytecode
+}
+
+cd hello-world
+echo "javac Main.java"
+javac Main.java
+pretty Main
+
+build_wordcount() {
+    CDH=../../../jvmc-libs/cdh-4.6
+
+    echo
+    echo "javac WordCount.java -cp"
+    echo "    $CDH/hadoop-annotations.jar"
+    echo "    $CDH/hadoop-common.jar"
+    echo "    $CDH/hadoop-mapreduce-client-common.jar"
+    echo "    $CDH/hadoop-mapreduce-client-core.jar"
+    javac WordCount.java -cp \
+$CDH/hadoop-annotations.jar:\
+$CDH/hadoop-common.jar:\
+$CDH/hadoop-mapreduce-client-common.jar:\
+$CDH/hadoop-mapreduce-client-core.jar
+
+    pretty WordCount
+    pretty WordCount\$IntSumReducer
+    pretty WordCount\$TokenizerMapper
+
+    echo
+    echo "creating WordCount.jar"
+    jar cvfe WordCount.jar WordCount WordCount*.class
+}
+
+cd ../wordcount
+build_wordcount
+
+cd ../nongeneric-wordcount
+build_wordcount
