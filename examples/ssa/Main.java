7bc97ce6f571084c137aecb919011f80aa8f59d8
Jacob Stanley <jacob@stanley.io>
2015-03-02 23:42:20 +1100

    Ensure last float is used

--- examples/ssa/Main.java
+++ examples/ssa/Main.java
@@ -8,7 +8,7 @@ public final class Main
         float f2 = f1 + f1;
         float f3 = f1 * f1;
         float f4 = f3 - f2;
-        String str = Float.toString(f2);
+        String str = Float.toString(f4);
         PrintStream out = System.out;
         out.println(str);
     }
